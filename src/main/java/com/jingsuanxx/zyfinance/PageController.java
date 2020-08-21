41f79dd74d27b2265c0a9adf51612606225b800a
qingfeng <844539509@qq.com>
2020-08-21 18:02:07 +0800

    no message

--- src/main/java/com/jingsuanxx/zyfinance/PageController.java
+++ src/main/java/com/jingsuanxx/zyfinance/PageController.java
@@ -1,12 +1,16 @@
 package com.jingsuanxx.zyfinance;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/page")
 public class PageController {
 
     @RequestMapping("/{path}")
     public String index(@PathVariable() String path) {
+        System.out.println("PageController");
         return path;
     }
 
